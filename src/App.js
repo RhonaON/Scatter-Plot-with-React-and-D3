8279589be04992e26b964ad13fc9caf369be1c55
Rhona O'Neill <rhonaon@gmail.com>
2022-05-02 17:06:17 +1200

    Move fetchUrl function to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { useState, useCallback } from 'react'
+import * as d3 from 'd3'
 
 const width = 960
 const height = 500
 const circleRadius = 30
 const initialMousePosition = { x: width / 2, y: height / 2 }
+
+const fetchText = async (url) => {
+  const response = await fetch(url)
+  return await response.text()
+}
+
+const csvUrl =
+  'https://gist.githubusercontent.com/RhonaON/a43e64b04c7d3c6d79a06457723793cd/raw/cssNamedColours.csv'
+
+fetchText(csvUrl).then((text) => {
+  console.log(d3.csvParse(text))
+})
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState(initialMousePosition)
