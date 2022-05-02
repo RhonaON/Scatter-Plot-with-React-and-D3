443b892dcc57f995cf8ee620ab27ea69f5fa7ddf
Rhona O'Neill <rhonaon@gmail.com>
2022-05-02 16:07:56 +1200

    add useCallBack hook to mouse movement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState, useCallback } from 'react'
 
 const width = 960
 const height = 500
@@ -8,10 +7,13 @@ const initialMousePosition = { x: width / 2, y: height / 2 }
 
 const App = () => {
   const [mousePosition, setMousePosition] = useState(initialMousePosition)
-  const handleMouseMove = (event) => {
-    const { clientX, clientY } = event
-    setMousePosition({ x: clientX, y: clientY })
-  }
+  const handleMouseMove = useCallback(
+    (event) => {
+      const { clientX, clientY } = event
+      setMousePosition({ x: clientX, y: clientY })
+    },
+    [setMousePosition]
+  )
   return (
     <svg width={width} height={height} onMouseMove={handleMouseMove}>
       <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} />
