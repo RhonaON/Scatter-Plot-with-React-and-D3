50d25d2b0ede3a2c0685e922e83fc0d7a3dd3f02
Rhona O'Neill <rhonaon@gmail.com>
2022-05-02 14:35:38 +1200

    Create circle starting point

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div>
-      <header></header>
-    </div>
-  )
-}
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const width = 960
+const height = 500
+const circleX = width / 2
+const circleY = height / 2
+const circleRadius = 30
+
+const App = () => (
+  <svg width={width} height={height}>
+    <circle cx={circleX} cy={circleY} r={circleRadius} />
+  </svg>
+)
 
 export default App
