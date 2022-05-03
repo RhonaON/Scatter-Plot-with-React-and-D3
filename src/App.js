b6ec6905d5971c9891abf02f1026a43c78ccc302
Rhona O'Neill <rhonaon@gmail.com>
2022-05-03 17:12:39 +1200

    Create useEffect, remove message fxn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import * as d3 from 'd3'
-import '../src/styles.css'
+import { message } from './Message'
 
 const csvUrl =
   'https://gist.githubusercontent.com/RhonaON/a43e64b04c7d3c6d79a06457723793cd/raw/cssNamedColours.csv'
-
-const message = (data) => {
-  let message = ''
-  message = message + Math.round(d3.csvFormat(data).length / 2670) + ' kb\n'
-  message = message + data.columns.length + ' columns\n'
-  message = message + data.length + ' rows\n'
-  return message
-}
 
 const App = () => {
   // initial state of null means data hasn't been loaded yet
   const [data, setData] = useState(null)
 
-  d3.csv(csvUrl).then((data) => {
-    setData(data)
-  })
+  useEffect(() => {
+    d3.csv(csvUrl).then(setData)
+  }, [])
 
   return <pre>Data is: {data ? message(data) : 'loading'}</pre>
 }
