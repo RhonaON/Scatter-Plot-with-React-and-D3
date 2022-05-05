feb4448bc4a5fb7bce26078e0cd1583fc3b906dc
Rhona O'Neill <rhonaon@gmail.com>
2022-05-05 15:17:33 +1200

    move relevant comment code from App.js

--- src/useData.js
+++ src/useData.js
@@ -4,6 +4,7 @@ import { csv } from 'd3'
 const csvUrl =
   'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
 
+// initial state of null means data hasn't been loaded yet
 export const useData = () => {
   const [data, setData] = useState(null)
   // the built in '+d' unary convention is the popular alternative to parseFloat for turning strings into numbers
