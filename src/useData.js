69e9cba3145934963fab79053fc045d25c03c523
Rhona O'Neill <rhonaon@gmail.com>
2022-05-06 15:54:26 +1200

    Change URL and logic for new file

--- src/useData.js
+++ src/useData.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { csv } from 'd3'
 
 const csvUrl =
-  'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv'
+  'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
 
 // initial state of null means data hasn't been loaded yet
 export const useData = () => {
@@ -10,13 +10,13 @@ export const useData = () => {
   // the built in '+d' unary convention is the popular alternative to parseFloat for turning strings into numbers
   useEffect(() => {
     const row = (d) => {
-      d.Population = +d['2020'] * 1000
+      d.sepal_length = +d.sepal_length
+      d.sepal_width = +d.sepal_width
+      d.petal_length = +d.petal_length
+      d.petal_width = +d.petal_width
       return d
     }
 
-    csv(csvUrl, row).then((data) => {
-      setData(data.slice(0, 10))
-    })
+    csv(csvUrl, row).then(setData)
   }, [])
-  return data
 }
